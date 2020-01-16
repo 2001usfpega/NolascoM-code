a750f6215e40715aaaa0b504a340504a56b8aee7
Marielle Nolasco <marielle.nolasco@revature.com>
2020-01-16 17:53:20 -0500

    Showing example of how to use repo

---
+++ RepoExample/src/com/revature/repoexample/RepoExample.java
@@ -0,0 +1,10 @@
+package com.revature.repoexample;
+
+public class RepoExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
